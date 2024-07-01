252e53ba8984d04f6c5afc43b305d287fbdc2282
khoabt94 <khoa.bui@buymed.com>
2024-07-01 15:18:41 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import Modal from './components/Modal.vue'
+export default {
+    name: 'App',
+    components: {
+      Modal,
+    },
+    data() {
+      return {
+        name: 'Khoa'
+      }
+    }
+}
+</script>
+
+<template>
+  <h1>Hello World {{ name }}</h1>
+  <Modal/>
+</template>
+
+<style>
+
+</style>
